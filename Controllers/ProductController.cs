dd38f4e7ef37638eb6bb9de9132411b7f2c4e8fa
SavenkovSS <Cae_2003@mail.ru>
2024-08-23 05:15:26 +0700

    AddToRepository

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using Market.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost("postProducts")]
+        public IActionResult PutProducts([FromQuery] string name, string description, int categoryId, double cost)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (!context.Products.Any(x => x.Name.ToLower().Equals(name)))
+                    {
+                        Product product = new Product()
+                        {
+                            Name = name,
+                            Description = description,
+                            CategoryId = categoryId,
+                            Cost = cost
+                        };
+                        context.Add(product);
+                        context.SaveChanges();
+
+                        return Ok(product.Id);
+                    }
+                    else
+                    {
+                        return StatusCode(409);
+                    }
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("deleteProduct")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id.Equals(id)))
+                    {
+                        var product = new Product()
+                        {
+                            Id = id
+                        };
+                        context.Products.Remove(product);
+                        context.SaveChanges();
+                    }
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("getProducts")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    var products = context.Products.Select(x => new Product()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description
+                    }).ToList();
+                    return products;
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPatch("editCost")]
+        public IActionResult EditCost(int id, double cost)
+        {
+            try
+            {
+                using (var context = new ProductContext())
+                {
+                    if (context.Products.Any(x => x.Id.Equals(id)))
+                    {
+                        var product = new Product()
+                        {
+                            Id = id,
+                            Cost = cost
+                        };
+                        context.Products.Update(product);
+                        context.SaveChanges();
+                    }
+                    return Ok();
+                }
+            }
+            catch
+            {
+                return StatusCode(404);
+            }
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Market.Models
+{
+    public class Product: BaseModel
+    {
+        public double Cost { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category? Category { get; set; }
+        public virtual List<Storage>? Storages { get; set; } = new List<Storage>();
+    }
+}
